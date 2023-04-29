266638af27bba06218e05bc8350b25d756839f2e
jonahgibbon <jonahgibbon@hotmail.com>
2023-04-29 13:24:31 +0100

    Final Submission

--- Gibbs-Sampler.m
+++ Gibbs-Sampler.m
@@ -1,17 +1,64 @@
 tic
-
-Date='20190401';
 Iterates=10000;
-M=200;
-a_1=1;
-a_2=0.5;
-Data=table2array(readtable(strcat(Date,'.csv'),"Format","auto"));
-% Data=table2array(readtable("TestData.csv","Format","auto"));
+M=50;
+A=[10,20];
+Quantiles=[0.025,0.1,0.5,0.9,0.975];
+Name='HumanTrafficking';
 
-K=size(Data,2)-1;
-n=sum(Data(:,end));
+Data=table2array(readtable(strcat(Name,'.csv'),"Format","auto"));
+N_Tb=Gibbs(Data(:,end),Iterates,M,A);
+N_Vals=N_Tb(Iterates-round(Iterates/2):Iterates);
+pd=fitdist(N_Vals,'kernel','Kernel','Normal');
+disp('_______________________')
+disp(strcat('Data used: ',Name))
+fprintf('Iterates = %u \n',Iterates)
+fprintf('M = %u \n',M)
+n=sum(Data(:,1));
+M=max(N_Vals);
+m=min(N_Vals);
+X=m:(M-m)/999:M;
+Y=pdf(pd,X);
+f=figure;
+plot(X,Y,'Color','Black')
+xlabel("N")
+exportgraphics(f,'Results.eps','ContentType','vector')
+fprintf('Min = %u \n',m)
+x=5*m/4-M/4:(3/2)*(M-m)/9999:5*M/4-m/4;
+y=pdf(pd,x);
+QuantAns=search(Quantiles,x,y);
+cnt=1;
+while cnt<=size(Quantiles,2)
+    fprintf('%.2f Quantile: %.1f\n',Quantiles(1,cnt),QuantAns(1,cnt))
+    cnt=cnt+1;
+end
+fprintf('Max = %u \n',M)
+fprintf('Mean = %.1f \n',mean(pd))
+fprintf('Std = %.1f \n',std(pd))
+fprintf('Observed cases n = %u \n',n)
+toc
+disp('_______________________')
 
-Z_Tb=zeros(Iterates,sum(Data(:,end)));
+function N_Tb = Gibbs(Data,Iterates,M,A)
+%INITIALIZE
+a_1=A(1);
+a_2=A(2);
+K=log2(size(Data,1)+1);
+n=sum(Data(:,1));
+DataAppend=zeros(2^K-1,K);
+cnt=1;
+cnt2=1;
+while cnt<=K
+    Comb=nchoosek(1:K,cnt);
+    cnt1=1;
+    while cnt1<=size(Comb,1)
+        DataAppend(cnt2,Comb(cnt1,:))=1;
+        cnt2=cnt2+1;
+        cnt1=cnt1+1;
+    end
+    cnt=cnt+1;
+end
+Data=[DataAppend Data];
+Z_Tb=zeros(Iterates,n);
 Theta_Tb=cell(size(Data,2)-1,M);
 cnt=1;
 while cnt<=size(Data,2)-1
@@ -38,11 +85,9 @@ while cnt<=M-1
     cnt=cnt+1;
 end
 Pi_Tb(1,M)=(1-1/(1+a_1*a_2))^(M-1);
-Z_Tb(1,:)=randsample(M,n,'true',Pi_Tb(1,:));%%%
-N_Tb(1,1)=80706;
+Z_Tb(1,:)=randsample(M,n,'true',Pi_Tb(1,:));
+N_Tb(1,1)=n;
 Omega_Tb(1,:)=round(Pi_Tb(1,:).*(4*n));
-
-
 
 cnt=2;
 while cnt<=Iterates
@@ -133,60 +178,33 @@ while cnt<=Iterates
 
     %SAMPLE FROM OMEGA
     Omega_Tb(cnt,:)=round(mnrnd(N_Tb(cnt)-n,Carry/sum(Carry)));
+
     cnt=cnt+1;
     fprintf(repmat('\b',1,Progress))
 end
-
-Data=N_Tb(Iterates-round(Iterates/2):Iterates);
-pd=fitdist(Data,'kernel','Kernel','Normal');
-disp('_______________________')
-disp(strcat('Data used: ',Date))
-fprintf('Iterates = %u \n',Iterates)
-fprintf('M = %u \n',M)
-
-M=max(Data);
-m=min(Data);
-X=m:(M-m)/999:M;
-x=5*m/4-M/4:(3/2)*(M-m)/9999:5*M/4-m/4;
-y=pdf(pd,x);
-Y=pdf(pd,X);
-figure
-plot(x,y,'Color','Black')
-xlabel("N")
-figure
-plot(X,Y,'Color','Black')
-xlabel("N")
-
-fprintf('Min = %u \n',m)
-
-Quant=cumtrapz(x,y);
-QuantCheck=[0.05,0.5,0.95];
-cnt=1;
-while cnt<=size(Quant,2)-1
-    cnt1=1;
-    while cnt1<=size(QuantCheck,2)
-        if and(Quant(cnt)<=QuantCheck(cnt1),Quant(cnt+1)>=QuantCheck(cnt1))==1
-            fprintf('%.2f Quantile = %.1f \n',QuantCheck(cnt1),...
-                round((cnt+1/2)/size(Quant,2)*(3/2*(M-m))+5*m/4-M/4,1))
-        end
-        cnt1=cnt1+1;
-    end
-    cnt=cnt+1;
 end
-fprintf('Max = %u \n',M)
-fprintf('Mean = %.1f \n',mean(pd))
-fprintf('Std = %.1f \n',std(pd))
-fprintf('Observed cases n = %u \n',n)
-toc
-disp('_______________________')
-beep on
-beep
-
 function counter = useful(number,Data)
 sum=0;
 counter=0;
 while sum<number
     sum=sum+Data(counter+1,end);
     counter=counter+1;
+end
+end
+function Answer = search(Quantiles,x,y)
+Quant=cumtrapz(x,y);
+Answer=zeros(size(Quantiles,1),1);
+cnt2=1;
+cnt=1;
+while cnt<=size(Quant,2)-1
+    cnt1=1;
+    while cnt1<=size(Quantiles,2)
+        if and(Quant(cnt)<=Quantiles(cnt1),Quant(cnt+1)>=Quantiles(cnt1))==1
+            Answer(cnt2)=round((cnt+1/2)/size(Quant,2)*(x(end)-x(1))+x(1),1);
+            cnt2=cnt2+1;
+        end
+        cnt1=cnt1+1;
+    end
+    cnt=cnt+1;
 end
 end
