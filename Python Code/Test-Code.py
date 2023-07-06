511273cf26dd1f926abe771d8186828bdd192116
jonahgibbon <jonahgibbon@hotmail.com>
2023-07-06 20:54:37 +0100

    Uploading python

---
+++ Python Code/Test-Code.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+n=5
+short_table=np.zeros((1,n+1)).astype(int)
+short_table=np.vstack([short_table,[1,1,24698,1,1,1]])
+short_table=np.vstack([short_table,[1,1,24698,1,1,1]])
+
+short_table=np.vstack([short_table,[1,1,24698,1,1,1]])
+
+short_table=np.vstack([short_table,[1,1,24698,1,1,1]])
+
+print(short_table)
